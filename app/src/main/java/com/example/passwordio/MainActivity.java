7bb206141b25557584400f950a4ec4556fda2985
jayakrishnan-jayu <jayu2442@gmail.com>
2022-12-08 23:07:24 +0530

    feat: custom action bar

--- app/src/main/java/com/example/passwordio/MainActivity.java
+++ app/src/main/java/com/example/passwordio/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.passwordio;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +13,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        Settings.setCustomActionBar(getSupportActionBar());
+        Settings settings = new Settings(getApplicationContext());
+
+        if (settings.isNewUser()) {
+            setContentView(R.layout.activity_create_account);
+        } else {
+            setContentView(R.layout.activity_login_account);
+        }
 
     }
 }
