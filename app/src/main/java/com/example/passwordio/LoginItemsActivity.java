cb034360497dd9c3affca041bac3156db0274372
Jayakrishnan Jayu <jayu2442@gmail.com>
2022-12-14 00:21:46 +0530

    minor fixes

--- app/src/main/java/com/example/passwordio/LoginItemsActivity.java
+++ app/src/main/java/com/example/passwordio/LoginItemsActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.passwordio.adapters.LoginAdapter;
@@ -18,22 +20,43 @@ public class LoginItemsActivity extends AppCompatActivity {
     LoginAdapter loginAdapter;
     DB db;
 
+    ImageView addButton, searchButton;
+    TextView actionBarTitle;
+    View supportActionBar;
+
+    private void update() {
+        db = new DB(getApplicationContext());
+        logins = db.allLogins();
+        loginAdapter = new LoginAdapter(logins);
+        rvLogin.setLayoutManager(new LinearLayoutManager(this));
+        rvLogin.setAdapter(loginAdapter);
+        count.setText(String.valueOf(logins.length));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_items);
 
-        db = new DB(getApplicationContext());
-        logins = db.allLogins();
+        Settings.setCustomActionBar(getSupportActionBar());
+        supportActionBar = getSupportActionBar().getCustomView();
+        actionBarTitle = supportActionBar.findViewById(R.id.tvTitle);
+        searchButton = supportActionBar.findViewById(R.id.actionBarSearch);
+        addButton = supportActionBar.findViewById(R.id.actionBarAdd);
 
-        loginAdapter = new LoginAdapter(logins);
+        actionBarTitle.setText("Logins");
+        addButton.setVisibility(View.VISIBLE);
+        searchButton.setVisibility(View.VISIBLE);
 
         rvLogin = findViewById(R.id.loginItemsRV);
-        rvLogin.setLayoutManager(new LinearLayoutManager(this));
-        rvLogin.setAdapter(loginAdapter);
-
         count = findViewById(R.id.loginItemsCount);
-        count.setText(String.valueOf(logins.length));
 
+        update();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        update();
     }
 }
--- app/src/main/java/com/example/passwordio/NoteItemsActivity.java
+++ app/src/main/java/com/example/passwordio/NoteItemsActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -17,24 +19,46 @@ public class NoteItemsActivity extends AppCompatActivity {
     DB db;
     TextView count;
     NoteAdapter noteAdapter;
+    ImageView addButton, searchButton;
+    TextView actionBarTitle;
+    View supportActionBar;
 
     public Note[] notes;
+
+    private void update() {
+        db = new DB(getApplicationContext());
+        notes = db.allNotes();
+        noteAdapter = new NoteAdapter(notes);
+        rvNote.setLayoutManager(new LinearLayoutManager(this));
+        rvNote.setAdapter(noteAdapter);
+        count.setText(String.valueOf(notes.length));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_items);
 
-        db = new DB(getApplicationContext());
-        notes = db.allNotes();
+        Settings.setCustomActionBar(getSupportActionBar());
 
-        noteAdapter = new NoteAdapter(notes);
+        supportActionBar = getSupportActionBar().getCustomView();
+        actionBarTitle = supportActionBar.findViewById(R.id.tvTitle);
+        searchButton = supportActionBar.findViewById(R.id.actionBarSearch);
+        addButton = supportActionBar.findViewById(R.id.actionBarAdd);
+
+        actionBarTitle.setText("Secure Notes");
+        addButton.setVisibility(View.VISIBLE);
+        searchButton.setVisibility(View.VISIBLE);
 
         rvNote = findViewById(R.id.noteItemsRV);
-        rvNote.setLayoutManager(new LinearLayoutManager(this));
-        rvNote.setAdapter(noteAdapter);
-
         count = findViewById(R.id.noteItemsCount);
-        count.setText(String.valueOf(notes.length));
+
+        update();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        update();
     }
 }
